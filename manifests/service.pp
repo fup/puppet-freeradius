d662bf8d7d08dda9861c2b505cd649393db911c3
James Fryman <james@frymanet.com>
2012-07-03 18:41:12 -0500

    removing 'hasstatus' for service

--- manifests/service.pp
+++ manifests/service.pp
@@ -2,7 +2,6 @@ class freeradius::service inherits freeradius::params {
   service { $freeradius::params::radius['service_name']:
     ensure     => running,
     enable     => true,
-    hasstatus  => true,
     hasrestart => true,
   }
 }
