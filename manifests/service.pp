e4fa527f39381b4f31c05a94b305ad4a8e15fbfe
James Fryman <james@frymanet.com>
2012-07-03 19:15:16 -0500

    doesn't have status for service

--- manifests/service.pp
+++ manifests/service.pp
@@ -2,6 +2,7 @@ class freeradius::service inherits freeradius::params {
   service { $freeradius::params::radius['service_name']:
     ensure     => running,
     enable     => true,
+    hasstatus  => false,
     hasrestart => true,
   }
 }
