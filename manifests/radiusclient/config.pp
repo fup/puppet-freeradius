05dcb2b15b728abcd8f9ed6313d91608612a8f4a
James Fryman <james@frymanet.com>
2012-07-03 19:12:11 -0500

    ensure port-id-map exists, or else funky things happen

--- manifests/radiusclient/config.pp
+++ manifests/radiusclient/config.pp
@@ -8,6 +8,14 @@ class freeradius::radiusclient::config {
     mode   => '0755',
   }
 
+
+  file { "${freeradius::params::radius['radiusclient_dir']}/port-id-map":
+    ensure => file,
+    owner  => 'root',
+    group  => 'root',
+    mode   => '0644',
+  }
+
   concat { "${freeradius::params::radius['radiusclient_dir']}/servers":
     owner => 'root',
     group => 'root',
